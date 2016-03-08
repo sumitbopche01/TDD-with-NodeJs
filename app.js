744a477e32ac3abc0c49dd983d70ed02173f688d
cianclarke <cian@cianclarke.com>
2016-03-08 13:17:43 -0500

    Add failing integration tests for addition and subtraction, along with dummy routes.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var express = require('express'),
+subtract = require('./lib/subtract'),
 app = express();
+
+app.get('/add', require('./routes/add'));
+app.get('/subtract', require('./routes/subtract'));
 
 app.listen(3000, function(){
   console.log('App is now listening');
